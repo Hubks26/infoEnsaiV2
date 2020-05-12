6f0070dfd16f2681ba3a174078987b6cdb9dcb5a
Besstial <62187633+Besstial@users.noreply.github.com>
2020-05-12 19:48:53 +0200

    Update gestionnaire.py

--- src/gestion/gestion_des_fichiers/gestionnaire.py
+++ src/gestion/gestion_des_fichiers/gestionnaire.py
@@ -5,13 +5,19 @@ file_name = "data.json"
 directory_data = "../media/files/"
 
 class Gestionnaire(Lecteur):
+	"""Cette classe configure les méthodes permettant la modification de la base de données"""
+	
 	def save_elm(self, elm):
+		"""méthode permettant d'ajouter un élément à la base de données"""
+		
 		chemin = elm.get_chemin_fichier()
 		liste_elm = self.read(chemin)
 		liste_elm.append(elm)
 		self.write(chemin, liste_elm)
 		
 	def suppr_elm(self, elm_a_supprimer):
+		"""méthode permettant de supprimer un élément de la base de données"""
+		
 		chemin = elm_a_supprimer.get_chemin_fichier()
 		liste_elm = self.read(chemin)
 		nouvelle_liste_elm = []
@@ -23,5 +29,7 @@ class Gestionnaire(Lecteur):
 		self.write(chemin, nouvelle_liste_elm)
 				
 	def update(self, donnees):
+		"""méthode permettant de mettre à jour le fichier"""
+		
 		with open(directory_data + file_name, "w") as json_file:
 			json.dump(donnees, json_file)
