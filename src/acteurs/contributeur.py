753f305e5badd96a789610d452f16973b5c0ac3d
Besstial <62187633+Besstial@users.noreply.github.com>
2020-05-12 17:46:42 +0200

    Update contributeur.py

--- src/acteurs/contributeur.py
+++ src/acteurs/contributeur.py
@@ -5,18 +5,31 @@ from gestion.elements_fichiers.compte import Compte
 from gestion.gestion_des_fichiers.gestionnaire import Gestionnaire
 
 class Contributeur(Consultant):
+	""" Cette classe ne représente pas un acteur.
+	Elle permet aux acteurs qui en hériteront, de se connecter afin d'acceder à 
+	leurs différentes tâches"""
+	
 	def __init__(self):
+		"""Le statut de cette classe est None"""
+		
 		super().__init__()
 		self.est_connecte = False
 		self.statut = None
 		
 	def verification_connexion(self):
+		"""Méthode qui permet de savoir si l'acteur est connecté ou non.
+		S'il ne l'est pas, il renvoie un message d'erreur"""
+		
 		if not self.est_connecte:
 			input("\nVEUILLEZ D'ABORD VOUS CONNECTER.\nAppuyez sur entrer pour continuer.")
 			return False
 		return True
 	
 	def se_connecter(self, contenu):
+		"""Cette méthode permet à l'acteur de se connecter.
+		L'individu entre son pseudo et son mot de passe. Il sera comparé au 
+		gestionnaire des comptes."""
+		
 		gestionnaire = Gestionnaire()
 		liste_des_comptes = gestionnaire.read(Compte().get_chemin_fichier())
 		
