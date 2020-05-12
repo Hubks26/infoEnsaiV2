dad33092b4f008a3298224b3c1c0c0370e926df2
Besstial <62187633+Besstial@users.noreply.github.com>
2020-05-12 19:01:48 +0200

    Update data_base.py

--- src/gestion/elements_fichiers/data_base.py
+++ src/gestion/elements_fichiers/data_base.py
@@ -2,6 +2,9 @@ import json
 from gestion.elements_fichiers.elm_fichier import Elm_Fichier
 
 class Data_Base(Elm_Fichier):
+	"""Cette classe permet d'importer le jeu de données.
+	les diverses informations sur les pays seront dans le fichier qui se nomme 'donnees'"""
+	
 	def __init__(self):
 		super().__init__("data.json")
 		
