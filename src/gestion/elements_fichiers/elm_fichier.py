ad79ecdfa99f20ee154b81a74f0cddc3f55cf678
Besstial <62187633+Besstial@users.noreply.github.com>
2020-05-12 18:54:06 +0200

    Update elm_fichier.py

--- src/gestion/elements_fichiers/elm_fichier.py
+++ src/gestion/elements_fichiers/elm_fichier.py
@@ -1,8 +1,13 @@
 
 class Elm_Fichier:
+	"""Cette classe permet de trouver les différents fichiers qui seront utilisés 
+	par l'application comme la base de données"""
+	
 	def __init__(self, nom_fichier):
 		self.chemin = "../media/files/"
 		self.nom_fichier = nom_fichier
 
 	def get_chemin_fichier(self):
+		"""Cette méthode renvoie le chemin d'un fichier"""
+		
 		return self.chemin + self.nom_fichier
