a41644e03b0aadabd5c98d7379ee94aee0b43cda
Besstial <62187633+Besstial@users.noreply.github.com>
2020-05-12 19:38:49 +0200

    Update lecteur.py

--- src/gestion/gestion_des_fichiers/lecteur.py
+++ src/gestion/gestion_des_fichiers/lecteur.py
@@ -1,10 +1,13 @@
 import pickle
 
 class Lecteur:
+	"""Cette classe s'occupe de lire et d'écrire dans un fichier correspondant"""
+	
 	def __init__(self):
 		pass
 	
 	def read(self, chemin_du_fichier):
+		"""Permet de lire le contenu du fichier"""
 		try:
 			with open(chemin_du_fichier, 'rb') as fichier:
 				unpickler = pickle.Unpickler(fichier)
@@ -15,6 +18,8 @@ class Lecteur:
 		return contenu
 	
 	def write(self, chemin_du_fichier, contenu):
+		"""permet d'écrire du contenu dans le fichier"""
+		
 		with open(chemin_du_fichier, 'wb') as fichier:
 			pickler = pickle.Pickler(fichier)
 			contenu = pickler.dump(contenu)
