195951be5017864aa33b1876080896db7fe01c3e
PDUGOR <62187582+PDUGOR@users.noreply.github.com>
2020-05-13 11:52:30 +0200

    Update derouleur.py

--- src/affichage/derouleur.py
+++ src/affichage/derouleur.py
@@ -2,9 +2,8 @@ from affichage.menu_ouvert import Menu_Ouvert
 from affichage.gestionnaire_menus import Gestionnaire_des_Menus
 
 class Derouleur:
-	# Un dérouleur permet d'afficher les messages de base d'une part
-	# et de dérouler les différents menus les uns à la suite des autres d'autre part.
-
+	# Un dérouleur permet d'afficher les messages de base,
+	# et de dérouler les différents menus les uns à la suite des autres.
 	def __init__(self):
 		pass
 
@@ -20,12 +19,12 @@ class Derouleur:
 		with open('../media/assets/border.txt', 'r', encoding="utf-8") as asset:
 			print(asset.read())
 			
-	def derouler(self): # Méthode permettant de dérouler les menus en commençant avec le menu initial.
+	def derouler(self): # Méthode permettant de dérouler les menus en commençant par le menu initial.
 		gestionnaire = Gestionnaire_des_Menus()
 		menu_actuel = Menu_Ouvert(gestionnaire.contenu_du_menu_initial)
 		
 		while menu_actuel: 
-			# La boucle s'arrète après être tombée sur un menu fermé. En effet, le fait 
+			# La boucle s'arrête après être tombée sur un menu fermé. En effet, le fait 
 			# de "run" un menu fermé renvoie None, ce qui permet de sortir de la boucle.
 			
 			self.bordure() # On affiche une bordure pour séparer les menus
